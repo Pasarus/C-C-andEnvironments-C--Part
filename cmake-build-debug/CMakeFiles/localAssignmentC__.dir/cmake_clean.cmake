8c2854aa6c8de123fc3367bc251e8da1da77f6b4
Samuel Jones <samjones714@gmail.com>
2017-12-07 15:57:23 +0000

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/localAssignmentC__.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/localAssignmentC__.dir/main.cpp.obj"
+  "CMakeFiles/localAssignmentC__.dir/Location.cpp.obj"
+  "localAssignmentC__.pdb"
+  "localAssignmentC__.exe"
+  "localAssignmentC__.exe.manifest"
+  "liblocalAssignmentC__.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/localAssignmentC__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include "Location.h"
+
+int main() {
+    /*
+    std::cout << "Great circle test" << std::endl;
+
+    Location aber(52.416090, -4.058268);
+    Location snowdon(53.0685, -4.0763);
+
+    std::cout << "Aberystwyth " << aber << std::endl;
+    std::cout << "Snowdon " << aber << std::endl;
+
+    std::cout << "Distance: " << aber.distance(snowdon) << std::endl;
+
+    std::cout << "Aber latitude: " << aber.getLatitude() << std::endl;
+
+     */
+    return 0;
+}
